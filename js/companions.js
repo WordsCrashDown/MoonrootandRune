e3577047042f01d6f0292f8ae1638eb5f763bf53
WordsCrashDown <justinea.dean@gmail.com>
2025-07-21 13:15:25 -0400

    Create companions.js
    Plant Companion Mapping for Moonroot & Rune Atelier, LLC website.

---
+++ js/companions.js
@@ -0,0 +1,6 @@
+const companionMap = {
+  basil: { companions: ['tomato', 'oregano'], enemies: ['sage'] },
+  tomato: { companions: ['basil', 'marigold'], enemies: ['potato'] },
+  rosemary: { companions: ['sage'], enemies: ['mint'] },
+  mint: { companions: ['cabbage'], enemies: ['rosemary'] }
+};
